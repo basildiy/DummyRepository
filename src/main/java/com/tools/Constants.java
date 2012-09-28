daac9dfbce03003e881cb481f5110c4fe5804a37
vasileteoca <basildiy@gmail.com>
2012-09-28 15:07:48 +0300

    sdadas

--- src/main/java/com/tools/Constants.java
+++ src/main/java/com/tools/Constants.java
@@ -11,4 +11,12 @@ public class Constants {
 
 	public static final String BASE_URL = "http://youtube.com";
 
+
+	
+	public void doodle(){
+		System.out.println("bla2");
+		System.out.println("bla1");
+		System.out.println("bla");
+		System.out.println("bla3");
+	}
 }
